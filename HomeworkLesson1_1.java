9c8dd1e8fbba877f57c47fc69fd309c82bf64df6
karinakomarovich <karina.komarovich@gmail.com>
2022-06-16 16:20:32 +0300

    Delete HW1 tasks from the lesson2 branch

--- HomeworkLesson1_1.java
+++
@@ -1,6 +0,0 @@
-public class HomeworkLesson1_1 {
-    public static void main(String[] args){
-        String[] HelloW = new String[]{"H","e","l","l","o", "", "W", "o", "r", "l", "d", "!"};
-        for (String s : HelloW) { System.out.println(s); }
-    }
-}
--- HomeworkLesson1_2.java
+++
@@ -1,9 +0,0 @@
-public class HomeworkLesson1_2 {
-    public static void main(String[] args){
-        int[] numbers = new int[]{0,1,2,3,4,5,6,7,8,9};
-        for(int i: numbers){
-            System.out.print(i);
-            System.out.print(" ");
-        };
-    }
-}
