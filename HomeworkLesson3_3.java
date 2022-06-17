ed5c265f62a0370d60275ade34500f59db1a91dc
karinakomarovich <karina.komarovich@gmail.com>
2022-06-17 10:46:13 +0300

    feat: add HW3 third and fourth tasks

--- HomeworkLesson3_3.java
+++ HomeworkLesson3_3.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class HomeworkLesson3_3 {
-}
+public class HomeworkLesson3_3 {
+    public static void main(String[] args){
+        int array[] = new int[]{2, 3, 1, 1, 4};
+        for (int j = array.length - 1; j >= 0; j--){
+            System.out.print(array[j]);
+        };
+    };
+};
--- HomeworkLesson3_4.java
+++ HomeworkLesson3_4.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class HomeworkLesson3_4 {
+public class HomeworkLesson3_4 {
+    public static void main(String[] args){
+        int arr[] = new int[]{1, 0, 3, 2, 8, 1, 2, 3, 4};
+        int min = 9;
+        int max = 0;
+        for (int i: arr) {
+            if( arr[i] > max) max = arr[i];
+            if( arr[i] < min) min = arr[i];
+        }
+        int sum = min + max;
+        System.out.println(min + " + " + max + " = " + sum);
+    }
 }
