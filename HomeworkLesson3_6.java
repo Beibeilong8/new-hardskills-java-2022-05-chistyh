b3438cb1767cf542b1217a7eff9dc8c8e31bfd7d
karinakomarovich <karina.komarovich@gmail.com>
2022-06-17 19:29:16 +0300

    fix: refactoring

--- HomeworkLesson3_6.java
+++ HomeworkLesson3_6.java
@@ -5,14 +5,10 @@ public class HomeworkLesson3_6 {
         for(int i = 0; i < arr.length; i++){
             arr[i] = "*";
             if (i <= arrCenter ){
-                for(int j = i; j > 0; j--){
-                    arr[i] += "     *";
-                }
+                for( int j = i; j > 0; j-- ) arr[i] += "     *";
             }
-            else if (i > arrCenter ){
-                for(int j = i - arrCenter; j <= arrCenter; j++){
-                    arr[i] += "     *";
-                }
+            else {
+                for( int j = i - arrCenter; j <= arrCenter; j++ ) arr[i] += "     *";
             }
             System.out.println(arr[i]);
         }
